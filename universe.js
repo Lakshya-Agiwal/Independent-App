1ed84ccdb910b2a57a8d7e61e09122278d4237bb
Lakshya-Agiwal <85127186+Lakshya-Agiwal@users.noreply.github.com>
2022-03-11 20:09:55 +0530

    Add files via upload

---
+++ universe.js
@@ -0,0 +1,34 @@
+function nav_hole()
+{
+    window.location="black_hole.html";
+}
+function nav_g()
+{
+    window.location="galaxies.html";
+}
+function nav_st()
+{
+    window.location="stars.html";
+}
+function nav_ne()
+{
+    window.location="nebula.html";
+}
+function nav_dm()
+{
+    window.location="dark_matter.html";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
